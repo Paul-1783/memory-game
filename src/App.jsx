9677de37c94d4d739f3805876e3ec021a729dd2e
Paul-1783 <paulschmitt144@gmail.com>
2026-05-25 16:24:17 +0200

    set up project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { useState } from 'react'
 import './App.css'
+import { MainArea } from './components/mainArea.jsx';
+import { Result } from './components/result.jsx';
+import { Card } from './components/card.jsx';
+
+
 
 function App() {
   const [countAttempts, setCountAttempts] = useState(0)
